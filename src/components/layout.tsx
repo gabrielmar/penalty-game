03f50c54344ee8683fde082e705601257620bb82
GabrielM <profissional.gabrielm@gmail.com>
2023-09-04 18:58:46 -0300

    feat: remove default style, pass props

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,16 +1,26 @@
-import { ReactNode } from 'react';
+import { HTMLAttributes, ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 import { Header } from './header';
 
-type LayoutProps = {
+type LayoutProps = HTMLAttributes<HTMLDivElement> & {
+  ignoreStyle?: boolean;
+  className?: string;
   children?: ReactNode;
 };
 
-export function Layout({ children }: LayoutProps) {
+export function Layout({ ignoreStyle, className, children, ...props }: LayoutProps) {
   return (
     <div className="relative flex min-h-screen flex-col">
       <Header />
-      <div className="mx-auto my-8 flex w-full max-w-screen-lg grow flex-col justify-center px-6 md:my-16">
+      <div
+        className={twMerge(
+          !ignoreStyle &&
+            'mx-auto my-8 flex w-full max-w-screen-lg grow flex-col justify-center px-6 md:my-16',
+          className,
+        )}
+        {...props}
+      >
         {children}
       </div>
     </div>
